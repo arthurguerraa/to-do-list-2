c17f1030812c9229fe11a7c25e09047d3b50bccd
arthurguerraa <127884471+arthurguerraa@users.noreply.github.com>
2025-06-12 18:35:28 -0300

    javascript e css att

---
+++ script.js
@@ -0,0 +1,37 @@
+const campo = document.getElementById('campo-tarefa');
+const ul = document.getElementById('lista');
+
+function adicionarTarefa(){
+    const texto = campo.value.trim();
+    if(texto === '') return;
+
+    const li = document.createElement('li');
+
+    const span = document.createElement('span');
+    span.textContent = texto;
+
+    const btnRemove = document.createElement('button');
+    btnRemove.textContent = 'X';
+    btnRemove.classList.add('btn-remove');
+
+    li.addEventListener('click', () =>{
+        li.classList.toggle('completo');
+    });
+
+    btnRemove.addEventListener('click', () =>{
+        li.remove();
+    });
+
+    li.appendChild(span);
+    li.appendChild(btnRemove);
+    ul.appendChild(li);
+
+    campo.value = '';
+    campo.focus();
+}
+
+campo.addEventListener('keydown', (event) =>{
+    if(event.key === 'Enter'){
+        adicionarTarefa();
+    }
+});
